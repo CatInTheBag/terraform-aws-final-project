48cf44c77b2e85365792a38d035a02efa38aa94a
CatInTheBag <vassil.bachvarov@gmail.com>
2024-01-21 13:33:43 +0200

    Update entry-script.sh

--- entry-script.sh
+++ entry-script.sh
@@ -2,27 +2,17 @@
 set -e  # Exit script on any command that returns a non-zero status
 set -x  # Enable debugging
 
-LOG_FILE="$HOME/logfile.log"
-
-# Function for logging
-log() {
-  local timestamp
-  timestamp=$(date +"%Y-%m-%d %T")
-  echo "[$timestamp] $1" >> "$LOG_FILE"
-}
-
 # Update and upgrade the system
-log "Updating and upgrading the system..."
 sudo apt update && sudo apt upgrade -y
 
 # Install Docker
-log "Installing Docker..."
 sudo apt install -y docker.io
 sudo systemctl start docker
 sudo usermod -aG docker $(whoami)
 
 # Install Ansible
-log "Installing Ansible..."
+echo "Before Ansible Installation"
 sudo apt install -y ansible
+echo "After Ansible Installation"
 
-log "Script execution completed successfully."
+echo "$(ansible --version)"
