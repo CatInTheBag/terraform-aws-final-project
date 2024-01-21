a7a0a22c422db52c1394e46acbfda1b57774753b
CatInTheBag <vassil.bachvarov@gmail.com>
2024-01-21 14:47:09 +0200

    Update entry-script.sh

--- entry-script.sh
+++ entry-script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Change to the home directory
+cd ~
+
 # Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
 
@@ -9,4 +12,12 @@ sudo systemctl start docker
 sudo usermod -aG docker $(whoami)
 
 # Install Ansible
-sudo apt install -y ansible
+sudo apt install ansible
+
+# Export the RUNNER_TOKEN environment variable
+export RUNNER_TOKEN="${var.runner_token}"
+
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
+echo "29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  actions-runner-linux-x64-2.311.0.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
