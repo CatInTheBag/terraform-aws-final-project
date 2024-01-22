75242b0c9baf4ce1b247e1f62ded586f66885705
CatInTheBag <vassil.bachvarov@gmail.com>
2024-01-22 22:49:12 +0200

    Update main.tf

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -75,8 +75,9 @@ resource "aws_instance" "myapp-ec2" {
       "curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz",
       "echo '29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  actions-runner-linux-x64-2.311.0.tar.gz' | shasum -a 256 -c",
       "tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz",
-      "./config.sh --url https://github.com/CatInTheBag/terraform-aws-final-project --token ${var.runner_token} --labels self-hosted,ubuntu,ec2 --name aws-ec2 --ephemeral --unattended",
-      "./run.sh"
+      "./config.sh remove --token ${var.runner_token}",
+      "./config.sh --url https://github.com/CatInTheBag/terraform-aws-final-project --token ${var.runner_token} --labels self-hosted,ubuntu,ec2 --name aws-ec2 --unattended --replace aws-ec2",
+      "./run.cmd &"
     ]
   }
 
